5660d3fbf2b893cb84b9d6a27ef976a1c872e9cf
Pintu Ray <90565220+Zenpinz@users.noreply.github.com>
2024-12-28 08:45:42 +0530

    Delete Convert_boolean_value _into_yesno.py

--- Convert_boolean_value _into_yesno.py
+++
@@ -1,6 +0,0 @@
-# Description:
-# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
-
-#solution:
-def bool_to_word(boolean):
-    return 'Yes' if boolean else 'No'
